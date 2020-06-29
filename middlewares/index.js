20de931ad2d841c289330fb3e02dfe7adabaef97
faramarz monazami haghighat <51343277+fafamnzm@users.noreply.github.com>
2020-06-29 20:49:20 +0430

    minor bug fix

--- middlewares/index.js
+++ middlewares/index.js
@@ -3,20 +3,26 @@ const jwt = require("jsonwebtoken")
 module.exports.tokenVerify = async (req) => {
   // token verification
   // Token Format: Bearer <token>
-  const token = req.headers.authorization
-    ? req.headers.authorization.split(" ")[1]
-    : ""
-  if (token === "") {
-    req.isAuth = false
-    return req
-  }
   try {
+    const token = req.headers.authorization
+      ? req.headers.authorization.split(" ")[1]
+      : ""
+    if (token === "") {
+      req.isAuth = false
+      return next()
+    }
     const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET)
+    if (!decodedToken) {
+      req.isAuth = false
+      return next()
+    }
     req.isAuth = true
     req.decodedToken = decodedToken
-    return req
+    return next()
   } catch (err) {
+    // throw err
+    console.log(err)
     req.isAuth = false
-    return req
+    return next()
   }
 }
